2342696582181e5af5c11ee50fbf72a188b6232b
jerry7991 <jerry31097@gmail.com>
2020-10-08 22:53:03 +0530

    First commit: create directory and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+app.listen(port, function (err)
+{
+    if (err) {
+        console.log("oops you got an error while loading the express on port number ", port);
+        return;
+    }
+    console.log(`cool! server is running on the port number : ${port}`);
+});v
